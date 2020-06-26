cf8c4682eb5cddfa2f909cb175638ee23b5f7a38
David Y. Stephenson <david@davidystephenson.com>
2020-06-26 13:31:23 +0200

    New dog

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect }  from 'react'
+import Teacher from './components/Teacher'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  const [count, setCount] = useState(0)
+  const [image, setImage] = useState('')
+  // const array = useState(0)
+  // const x = array[0]
+  // const setX = array[1]
+
+  async function getDog () {
+    const response = await axios
+      .get('https://dog.ceo/api/breeds/image/random')
+
+    console.log('response.data test:', response.data)
+
+    setImage(response.data.message)
+  }
+
+  function effect () { getDog() }  
+
+  useEffect(effect, [])
+
+  async function onClick () {
+    setCount(count + 1)
+
+    getDog()
+  }
+
+  const message = 'Goodbye'
+
+  return <div>
+    <img src={image} />
+    this has been clicked this many times: {count}
+    <button onClick={onClick}>Add</button>
+    <h1>{message}</h1>
+    <Teacher name='bruinsma' punctuation='!' />
+    <Teacher name='david' punctuation='...' />
+    <Teacher name='matias' punctuation='?' />
+  </div>
 }
 
 export default App;
---
+++ src/components/Teacher.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Teacher ({ name, punctuation }) {
+  return <p>{name}, we will miss you{punctuation}</p>
+}
